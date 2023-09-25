c617fcfc8a076b90b47a7037b826bc5777848e0d
Maurin Voshol <johannes.voshol@student.uantwerpen.be>
2023-09-25 23:59:06 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,126 @@
-# https-tunnel-server
+# Reverse Proxy HTTPS Server Setup Assistant
+
+## Introduction
 This project provides an automated and interactive installer aimed at simplifying the process of setting up a secure HTTPS server using `frp` and `Nginx`. The script is especially useful for serving HTTPS webpages from a local server situated behind a NAT or firewall.
+
+Setting up an HTTPS server over `frp` can be complex due to the requirement for the client to hold private certificate keys. However, transferring these keys from the server to the client is not optimal, as it's preferable for the server to handle HTTPS. To maintain a secure connection between the client and `frp` server, a TLS connection is used with a secret token. While `frp` can serve the websites in HTTPS, Nginx is utilized as an additional proxy for handling HTTPS.
+
+The primary goal of this project is to alleviate the complexities and time consumption involved in the manual setup by implementing an interactive installer, making the process straightforward and user-friendly.
+
+## Prerequisites
+### Public server:
+- A system with a public IP and available ports: 80 (HTTP), 443 (HTTPS), 7000 (TCP, bind with private server), and 8443 (TCP, FRP dashboard).
+- A registered domain name (e.g. `example.com`) that is configured to refer to the public IP of this system. This is required for obtaining certificates via [Let's Encrypt](https://letsencrypt.org/).
+
+### Private server (client behind a NAT):
+- A client serving HTTP (not HTTPS) requests on localhost:80.
+
+### Network Configuration:
+- During the setup, port 80 must be available and open to the internet for obtaining certificates via Let's Encrypt.
+- Ports 7001 (HTTP proxy) and 7002 (HTTPS proxy) must be available on the server for the Nginx proxy server to function properly. These can be altered in the code if necessary.
+
+## Installation
+Clone the repository to your local machine:
+```sh
+git clone https://github.com/m4urin/https-tunnel-server.git
+```
+Navigate to the cloned directory and run `install.sh`:
+```sh
+cd https-tunnel-server && sudo bash install.sh
+```
+
+This will take you through the process of setting up the server and the creation of the certificates.
+
+
+## Configuration
+
+The installer tries to handle the configuration, but can they can be adjusted later:
+
+### FRP (fast reverse proxy)
+The configuration files for `frp` are stored in `/opt/frp`:
+
+- For Server: `/opt/frp/frps.ini`
+- For Client: `/opt/frp/frpc.ini`
+
+You can modify these files to tweak the settings to your preference, and restart the service for the changes to take effect.
+
+### Certificates
+The certificates obtained from Let's Encrypt are stored in `/etc/letsencrypt/live/[your_domain_name]/`. 
+Let's Encrypt automatically sets up a timer to renew the certificates when close to expiration.
+
+### Ports
+The ports configured for Nginx proxy can be altered in the code, located at ports 7001 (HTTP proxy) and 7002 (HTTPS proxy) by default.
+These are not the ports for users or the clients, just between the server and the nginx instance.
+
+### Running the server
+
+After installation, if you selected to create a `systemctl` service during the installation process, the FRP server/client will start automatically upon system boot. 
+
+When changing `frps.ini`/`frpc.ini`  on the server/client, restart with:
+```sh
+sudo systemctl restart [frps/frpc]
+```
+and show the status with:
+```sh
+sudo systemctl status [frps/frpc]
+```
+
+If you did not select to create a service, you can start them manually by running the following commands:
+
+For Server:
+```sh
+sudo /opt/frp/frps -c /opt/frp/frps.ini
+```
+
+For Client:
+```sh
+sudo /opt/frp/frpc -c /opt/frp/frpc.ini
+```
+
+### Dashboard
+If you have configured the server, the FRP dashboard can be accessed through
+`https://[your_domain_name]:7500` 
+using the username `admin` and the password generated during the installation process.
+The username, password and port can be changed in `/opt/frp/frps.ini`
+
+### Nginx
+Nginx configuration for the server is stored in `/etc/nginx/sites-available/[your_domain_name]` and can be modified to suit your needs. 
+After modifying, ensure to reload Nginx to apply the changes:
+```sh
+sudo systemctl reload nginx
+```
+
+## Usage
+### Example
+```python
+#app.py
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=80)
+```
+
+`app.py` running on `localhost:80`:
+```sh
+sudo systemctl reload frpc
+python app.py
+```
+
+
+
+## Troubleshooting
+If you encounter any issues during installation or usage, please refer to the FRP [official documentation](https://github.com/fatedier/frp) and the Nginx [official documentation](http://nginx.org/en/docs/).
+
+For issues specifically related to this setup script, please check the existing issues or create a new one in this repository’s [Issue Tracker](https://github.com/m4urin/https-tunnel-server/issues). Thanks in advance!
+
+## Contributing
+If you would like to contribute anything to this project, feel free to submit a pull request. I welcome any bug fixes, and other contributions.
+
+## License
+This project is licensed under the Apache License, Version 2.0 - see the [LICENSE](LICENSE) file for details.
---
+++ install.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+# Brief Description of the script
+echo "This script automates the setup of FRP services with required packages and configurations."
+
+# Inform user about required packages
+echo "The following packages are required: openssl, certbot, nginx"
+read -p "Install dependencies? [Y/n]: " install_dependencies
+install_dependencies=${install_dependencies:-Y}  # Default to Y if no input is provided
+
+# Exit if user opts not to install dependencies
+if [[ ! "$install_dependencies" =~ ^[Yy]$ ]]; then
+    echo "Aborting due to unmet dependencies."
+    exit 1
+fi
+
+# Installing dependencies
+sudo apt-get update && sudo apt-get install -y certbot nginx openssl
+
+# Prompt user to choose service type
+read -p "Setup FPR [server/client]: " service_type
+if [[ "$service_type" != "server" && "$service_type" != "client" ]]; then
+    echo "Invalid service type, please select 'server' or 'client'. Aborting..."
+    exit 1
+fi
+
+# Set the other service type
+other_service=$([[ "$service_type" == "server" ]] && echo "client" || echo "server")
+
+# Prompt user for download link or use default
+read -p "FRP download link [default: https://github.com/fatedier/frp/releases/download/v0.51.3/frp_0.51.3_linux_amd64.tar.gz]: " download_link
+download_link=${download_link:-"https://github.com/fatedier/frp/releases/download/v0.51.3/frp_0.51.3_linux_amd64.tar.gz"}  # Default link
+
+# Download and extract the package
+file_name=$(basename "$download_link")
+cd "$(dirname "$0")" || { echo "Failed to change directory. Aborting..."; exit 1; }
+wget "$download_link" || { echo "Failed to download package. Aborting..."; exit 1; }
+mkdir -p /opt/frp
+tar -xvzf "$file_name" --strip-components=1 -C /opt/frp
+rm -f "$file_name"
+
+# Handling Authentication and Secret Token
+read -p "Secret token from $other_service [default: auto-generate]: " secret_token
+if [ -z "$secret_token" ]; then
+    secret_token=$(openssl rand -hex 32)
+    echo "Generated secret token: $secret_token"
+    echo "Please copy this token for $other_service configuration."
+fi
+
+
+# Server Configuration
+if [ "$service_type" == "server" ]; then
+    # Generate SSL Certificate
+    echo "Generating SSL certificates for HTTPS."
+    read -p "Domain name (e.g. example.com): " domain_name
+    if [ -z "$domain_name" ]; then
+        echo "Invalid domain name, cannot be empty. Aborting..."
+        exit 1
+    fi
+
+    echo "Ensure your domain name points to this IP, is public, and available on port 80."
+    read -p "Continue? [Y/n]: " continue_process
+    continue_process=${continue_process:-Y}  # Default to Y if no input is provided
+
+    if [[ ! "$continue_process" =~ ^[Yy]$ ]]; then
+        echo "Aborting..."
+        exit 1
+    fi
+
+    sudo certbot certonly --standalone -d "$domain_name" || { echo "Certbot failed. Aborting..."; exit 1; }
+
+    # Configure Dashboard
+    echo "Dashboard is accessible at 'https://$domain_name:7500'"
+    dashboard_pwd=$(openssl rand -hex 16)
+    echo "   username: admin"
+    echo "   password: $dashboard_pwd"
+
+    sed -e "s/SECRET_TOKEN/$secret_token/" -e "s/DASHBOARD_PWD/$dashboard_pwd/" templates/frps.ini > /opt/frp/frps.ini
+    echo "Secret token and dashboard password are configurable in '/opt/frp/frps.ini'"
+
+    sed "s/DOMAIN_NAME/$domain_name/g" templates/nginx_server.conf > /etc/nginx/sites-available/$domain_name
+    echo "Nginx proxy configuration is in '/etc/nginx/sites-available/$domain_name'"
+
+    # Activate Nginx Configuration
+    ln -s /etc/nginx/sites-available/$domain_name /etc/nginx/sites-enabled/
+    sudo nginx -t && sudo systemctl restart nginx || { echo "Nginx configuration test failed or unable to restart Nginx. Aborting..."; exit 1; }
+fi
+
+# Client Configuration
+if [ "$service_type" == "client" ]; then
+    sed -e "s/SECRET_TOKEN/$secret_token/" -e "s/DOMAIN_NAME/$domain_name/" templates/frpc.ini > /opt/frp/frpc.ini
+    echo "Secret token and domain name are configurable in '/opt/frp/frpc.ini'"
+fi
+
+# Offer to Create a Systemctl Service
+read -p "Would you like to create a systemctl service for your $service_type? [Y/n]: " create_service
+create_service=${create_service:-Y}  # Default to Y if no input is provided
+
+if [[ "$create_service" =~ ^[Yy]$ ]]; then
+    if [ "$service_type" == "server" ]; then
+        cp -f templates/frps.service /etc/systemd/system/frps.service
+        sudo systemctl daemon-reload && sudo systemctl enable frps && sudo systemctl start frps || {
+            echo "Failed to configure 'frps' service. Aborting..."
+            exit 1
+        }
+        echo "Service 'frps' created and can be configured with '/opt/frp/frps.ini'"
+    else
+        cp -f templates/frpc.service /etc/systemd/system/frpc.service
+        sudo systemctl daemon-reload && sudo systemctl enable frpc && sudo systemctl start frpc || {
+            echo "Failed to configure 'frpc' service. Aborting..."
+            exit 1
+        }
+        echo "Service 'frpc' created and can be configured with '/opt/frp/frpc.ini'"
+    fi
+else
+    echo "Systemctl service creation skipped."
+    if [ "$service_type" == "server" ]; then
+        echo "You can start the service manually by running the following command:"
+        echo "sudo /opt/frp/frps -c /opt/frp/frps.ini"
+    else
+        echo "You can start the service manually by running the following command:"
+        echo "sudo /opt/frp/frpc -c /opt/frp/frpc.ini"
+    fi
+fi
+
+echo "Installation and configuration completed successfully."
